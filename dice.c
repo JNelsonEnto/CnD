bf4ba26ee6a6647b355976d98c5818a93a936d7a
JNelsonEnto <johnathannelsonento@csus.edu>
2020-08-19 15:43:59 -0700

    Create dice.c

---
+++ dice.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int d2(){
+	return 1 + (rand() % 2);
+}
+
+int d3(){
+	return 1 + (rand() % 3);
+}
+
+int d4(){
+	return 1 + (rand() % 4);
+}
+
+int d6(){
+	return 1 + (rand() % 6);
+}
+
+int d8(){
+	return 1 + (rand() % 8);
+}
+
+int d12(){
+	return 1 + (rand() % 12);
+}
+int d20(){
+	return 1 + (rand() % 20);
+}
+
+int d100(){
+	return 1 + (rand() % 100);
+}
+
