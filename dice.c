ff132dbabd609a230ddbecf29a4e2789a538cf16
stryqwills <stryqwills@gmail.com>
2020-08-20 10:30:51 -0700

    Update dice.c

--- dice.c
+++ dice.c
@@ -2,33 +2,33 @@
 #include <stdlib.h>
 
 int d2(){
-	return 1 + (rand() % 2);
+	return 1 + (random() % 2);
 }
 
 int d3(){
-	return 1 + (rand() % 3);
+	return 1 + (random() % 3);
 }
 
 int d4(){
-	return 1 + (rand() % 4);
+	return 1 + (random() % 4);
 }
 
 int d6(){
-	return 1 + (rand() % 6);
+	return 1 + (random() % 6);
 }
 
 int d8(){
-	return 1 + (rand() % 8);
+	return 1 + (random() % 8);
 }
 
 int d12(){
-	return 1 + (rand() % 12);
+	return 1 + (random() % 12);
 }
 int d20(){
-	return 1 + (rand() % 20);
+	return 1 + (random() % 20);
 }
 
 int d100(){
-	return 1 + (rand() % 100);
+	return 1 + (random() % 100);
 }
 
